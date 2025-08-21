962cdf8927706c9895590e36495de3d128871ca7
Vaibhav Dange <vaibhavdange23@gmail.com>
2025-08-21 10:48:04 +0530

    served index.html file

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 5000;
 
+app.use(express.static(path.join(__dirname, ".")));
+
 app.get('/', (req, res) => {
-    res.json({ message: 'Hello from the backend API!' });
+    res.sendFile(path.join(__dirname, "index.html"));
 });
 
 app.listen(port, () => {
